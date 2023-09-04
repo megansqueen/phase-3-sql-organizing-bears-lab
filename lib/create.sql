2503aa70975ee8fde9958d9509248222fff03845
Megan <esler6724@gmail.com>
2023-09-03 18:23:40 -0700

    done

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+)
+
+-- creating bears table
+--   creates a bears table with a name field (FAILED - 1)
+--   creates a bears table with a age field (FAILED - 2)
+--   creates a bears table with a sex field (FAILED - 3)
+--   creates a bears table with a color field (FAILED - 4)
+--   creates a bears table with a temperament field (FAILED - 5)
+--   creates a bears table with an alive field (FAILED - 6)
+--   creates a bears table with an id as a primary key (FAILED - 7)
