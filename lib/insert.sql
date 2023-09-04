a59bf67423c606048eb32b9962c228a9d80fe6cf
Megan <esler6724@gmail.com>
2023-09-03 18:23:22 -0700

    commit tree a589b502fefaa5fe4d6506a22ef2c2a39aef3735

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'M', 'brown', 'friendly', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'brown', 'friendly', 'true');
+
+
+-- Get the tests in spec/insert_spec.rb to pass by writing code in the lib/insert.sql file. Input the following 8 bears (you can make up details about them, but make sex either 'M' or 'F'):
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (refer back to how to create a record that doesn't have one value)
+
+-- CREATE TABLE bears (
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER,
+-- sex TEXT,
+-- color TEXT,
+-- temperament TEXT,
+-- alive BOOLEAN
+-- )
